3c6264a558d6ceb47f8cf626354ee99e8b825988
yatharth-vataliya <yatharthvataliya@gmail.com>
2024-08-03 18:07:38 +0530

    Some modifications related to show hidden files and dotfiles in neo tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -20,6 +20,16 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          '.git',
+        },
+        never_show = {},
+      },
     },
   },
 }
