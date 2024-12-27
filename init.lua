92dab68177175abdd858b1bb327ffb9753290f51
yatharth-vataliya <yatharthvataliya@gmail.com>
2024-12-27 10:24:14 +0530

    some plugins enabled

--- init.lua
+++ init.lua
@@ -705,8 +705,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        php = { 'pint' },
-        blade = { 'blade-formatter' },
+        -- php = { 'pint' },
+        -- blade = { 'blade-formatter' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -925,7 +925,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
